f711772ca547b5ba71e5bc91b278482316dd2c3c
Leonardo <leonardongl@gmail.com>
2021-05-07 08:15:12 -0400

    ✨ Paginação

--- frontend/src/components/users/UserTable/index.tsx
+++ frontend/src/components/users/UserTable/index.tsx
@@ -6,6 +6,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
@@ -26,7 +27,19 @@ const useStyles = makeStyles({
 
 export default function CustomizedTables() {
   const [users, setUsers] = useState<IUser[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(5);
   const classes = useStyles();
+
+  const handleChangePage = (event: any, newPage: any) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, users.length - page * rowsPerPage);
   
   async function getUsers(): Promise<void> {
     const { data } = await api.get<IUser[]>(`users`);
@@ -53,11 +66,13 @@ export default function CustomizedTables() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {users.map(u => (
-            <TableRow key={u.name}>
-              <TableCell scope="row">{u.name}</TableCell>
-              <TableCell>{u.email}</TableCell>
-              <TableCell align="center">{u.phone}</TableCell>
+          {users
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((user, index) => (
+              <TableRow key={user.name}>
+              <TableCell scope="row">{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell align="center">{user.phone}</TableCell>
               <TableCell align="center">
                 <IconButton component="span" size="small" >
                   <EditIcon />
@@ -67,9 +82,23 @@ export default function CustomizedTables() {
                 </IconButton>
               </TableCell>
             </TableRow>
-          ))}
+            ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={users.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
     </TableContainer>
   );
 }
